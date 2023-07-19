00cb3d20b7beb1e5e48c714e3d11ad59de9f2b02
Rajat Yadav <es.rajatyadav@gmail.com>
2023-07-19 15:40:47 +0530

    Minor Error Fix

--- udemy_quiz_app/lib/question_summary.dart
+++ udemy_quiz_app/lib/question_summary.dart
@@ -9,6 +9,8 @@ class QuestionSummary extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(children: questionSummaryData.map((data) {
       return Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(((data['question_index'] as int) + 1).toString()),
           Column(children: [
@@ -17,7 +19,7 @@ class QuestionSummary extends StatelessWidget {
             Text(data['selectedOption'] as String, style: const TextStyle(fontFamily: 'JetBrainsReg', color: Colors.white),),
             Text(data['correctOption'] as String, style: const TextStyle(fontFamily: 'JetBrainsReg', color: Colors.white),),
           ],),
-        ],)
+        ],);
     }).toList(),);
   }
 }
